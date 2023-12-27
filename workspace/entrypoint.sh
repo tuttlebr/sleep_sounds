046fd8344627d5f76f08159e2bab81f603564d8c
Brandon Tuttle <btuttle@nvidia.com>
2023-12-27 12:09:37 -0500

    Added audio parameters as envs

--- workspace/entrypoint.sh
+++ workspace/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 clear
 rm -rf *.raw *.flac *.wav
+SAMPLERATE=16000
+NSEC=600
 
 for COLOR in white pink blue violet brown;
 do
     echo "Generating ${COLOR} noise..."
-    python3 soothing.py ${COLOR} -fs=44100 -nsec=600 -o ${COLOR}.raw
-    ffmpeg -f s16le -ar 44100 -ac 1 -i ${COLOR}.raw ${COLOR}.wav
+    python3 soothing.py ${COLOR} -fs=${SAMPLERATE} -nsec=${NSEC} -o ${COLOR}.raw
+    ffmpeg -f s16le -ar ${SAMPLERATE} -ac 1 -i ${COLOR}.raw ${COLOR}.wav
     echo
 done
